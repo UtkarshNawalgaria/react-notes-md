bb37d8e4561e87a13a7d60dc9210d1484af14fef
whimsicalartist123 <nawalgaria.utkarsh8@gmail.com>
2021-04-02 16:05:08 +0530

    added amplify auth, api and functions

---
+++ amplify/backend/function/reactnotesfn/src/index.js
@@ -0,0 +1,16 @@
+exports.handler = async (event) => {
+
+    if (event.requestContext.authorizer) {
+        console.log('claims: ', event.requestContext.authorizer.claims)
+    }
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+    },
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+  return response;
+};
